9c7f4c192d515b8ac32b6423288d7a3ce352b9b1
Dang <dangalpha78@gmail.com>
2024-11-01 21:11:16 +0700

    queue() self-sorted by prio, dequeue() get q[0]

--- src/queue.c
+++ src/queue.c
@@ -12,36 +12,38 @@ void enqueue(struct queue_t * q, struct pcb_t * proc) {
         if (q->size > MAX_QUEUE_SIZE){
                 return;
         }
-        q->proc[q->size] = proc;
-        q->size++;
+        else if (!q->size){
+                q->proc[0] = proc;
+                q->size++;
+        }
+        else {
+                int index = q->size;
+                while (index > 0 && q->proc[index - 1]->priority > proc->priority){
+                        q->proc[index] = q->proc[index - 1];
+                        index--;
+                }
+
+                q->proc[index] = proc;
+                q->size++;
+        }
+        //printf("helooooooooooo %d\n", q->size);
 }
 
 struct pcb_t * dequeue(struct queue_t * q) {
         /* TODO: return a pcb whose prioprity is the highest
          * in the queue [q] and remember to remove it from q
          * */
-        if (!empty(q)){
-                struct pcb_t * result = q->proc[0];
-                uint32_t highestPriority = result->priority;
-                int temp = 0;
+        struct pcb_t * result = NULL;
+        if (q->proc[0] != NULL){
+                result = q->proc[0];
 
-                for (int index = 0; index < q->size; index++){
-                        if (q->proc[index]->priority < highestPriority){
-                                highestPriority = q->proc[index]->priority;
-                                result = q->proc[index];
-                                temp = index;
-                        }
+                for (int i = 0; i < q->size - 1; i++){
+                        q->proc[i] = q->proc[i + 1];
                 }
-
-                for (int index = temp; index < q->size - 1; index++){
-                        q->proc[index] = q->proc[index + 1];
-                }
-
                 q->proc[q->size - 1] = NULL;
                 q->size--;
 
-                return result;
         }
-	return NULL;
+	return result;
 }
 
